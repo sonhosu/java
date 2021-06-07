b6f95caa1ca63dfdb1cd71beaa338c2c16fc4688
sonhosu <ghtn9398@nate.com>
2021-06-08 00:09:47 +0900

    # WARNING: head commit changed in the meantime
    1

---
+++ myTest1/src/main/java/son/testServlet.java
@@ -0,0 +1,53 @@
+package son;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class testServlet
+ */
+@WebServlet("/test")
+public class testServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public testServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		response.setContentType("text/html;charset=UTF-8");
+		String msg = "Hello World!";
+		PrintWriter out = response.getWriter();
+		out.println("<html lang='ko'>");
+		out.println("<head>");
+		out.println("<title>테스트</title><meta charset='UTF-8'>");
+		out.println("</head>");
+		out.println("<body>테스트입니다. <br><h2>" + msg + "</h2></body>");
+		out.println("</html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
